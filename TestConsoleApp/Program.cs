1857251fa8988ec231ec155af26d3d4676f3168f
Piotr Paul <piotr.paul@gmail.com>
2020-08-14 00:28:17 +0200

    Dane z DaneSzukajPodmioty i podstawowa implementacja DanePobierzPelnyRaport

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PortalApiGusApiRegonData;
+using System;
+using System.IO;
 
 namespace TestConsoleApp
 {
@@ -6,7 +8,21 @@ namespace TestConsoleApp
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            await PortalApiGusApiRegonData.UslugaBIRzewnPubl.ZalogujAsync("b65986a6300044a0b7fb");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByKrs("b65986a6300044a0b7fb", krs: "0000349095");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByKrsy("b65986a6300044a0b7fb", krsy: "0000349095,0000387233");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByNip("b65986a6300044a0b7fb", nip: "5730207319");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByNipy("b65986a6300044a0b7fb", nipy: "5730207319,5321052523");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByRegon("b65986a6300044a0b7fb", regon: "001337730");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByRegony9zn ("b65986a6300044a0b7fb", regony9zn: "001337730,366316484");
+            //await PortalApiGusApiRegonData.UslugaBIRzewnPubl.DaneSzukajPodmiotyAsyncByRegony14zn("b65986a6300044a0b7fb", regony14zn: "001337730,366316484");
+            //_ = await DaneSzukajPodmioty.DaneSzukajPodmiotyAsyncByKrs("b65986a6300044a0b7fb", krs: "0000349095");
+            //Inergis
+            //await DanePobierzPelnyRaport.DanePobierzPelnyRaportAsync("b65986a6300044a0b7fb", nip: "5730207319");
+            //Isk
+            //await DanePobierzPelnyRaport.DanePobierzPelnyRaportAsync("b65986a6300044a0b7fb", krs: "0000349095");
+            //Stylmeb
+            await DanePobierzPelnyRaport.DanePobierzPelnyRaportAsync("b65986a6300044a0b7fb", nip: "5321052523");
+            //5321052523
         }
     }
 }
