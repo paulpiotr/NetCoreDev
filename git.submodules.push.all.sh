6106d1d58bf88a0a3869ac719cdd1fcce1fc9df4
Piotr Paul <piotr.paul@gmail.com>
2020-10-26 23:31:01 +0100

    Import transakcji z plików XML z systemu ICASA i zmiana w wspólnej bibliotece - dodanie klasy pomocnika XML

---
+++ git.submodules.push.all.sh
@@ -0,0 +1,25 @@
+#! bash
+
+function submodulecheck {
+    cd $PWD;
+    remote_name=$(git remote -v | awk '{ print $1 }');
+    remote_url=$(git remote -v | awk '{ print $2 }');
+    if [[ $remote_url == *"http://tfs:8080/tfs"* ]]; then
+        git push originisk master
+    fi
+    if [[ $remote_url == *"https://github.com/paulpiotr"* ]]; then
+        git push origin master
+    fi
+}
+
+{
+    # try
+    export -f submodulecheck;
+    set -e;
+    submodulecheck
+    git submodule foreach bash -c 'submodulecheck';
+
+} || {
+    # catch
+    echo 'Error'
+}
