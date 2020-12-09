33419765a6b6d167ac4350c032ced5ab40917129
Piotr Paul <piotr.paul@gmail.com>
2020-12-09 11:58:15 +0100

    Merge zmian

--- git.submodules.push.all.sh
+++ git.submodules.push.all.sh
@@ -4,14 +4,18 @@ function submodulecheck {
     cd $PWD;
     remote_name=$(git remote -v | awk '{ print $1 }');
     remote_url=$(git remote -v | awk '{ print $2 }');
-    if [[ $remote_url == *"http://tfs:8080/tfs"* ]]; then
-        git push originisk master
-    fi
-    if [[ $remote_url == *"https://github.com/paulpiotr"* ]]; then
-        git push origin master
+    if [[ $remote_url != *"WebApplicationNetCoreDevDebug"* && $remote_url != *"WebApplicationNetCoreDevRelease"* ]]; then
+        if [[ $remote_url == *"http://tfs:8080/tfs"* ]]; then
+            git push originisk master
+        fi
+        if [[ $remote_url == *"https://tfs.isk.com.pl/tfs/ISK"* ]]; then
+            git push originisk master
+        fi
+        if [[ $remote_url == *"https://github.com/paulpiotr"* ]]; then
+            git push origin master
+        fi
     fi
 }
-
 {
     # try
     export -f submodulecheck;
